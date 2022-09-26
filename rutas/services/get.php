2e89c25cf5e1ef7a81d4f686b5dd05ccb17bb91b
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-09-26 17:51:41 -0500

    Terminación validación api por token

--- rutas/services/get.php
+++ rutas/services/get.php
@@ -1,8 +1,7 @@
 <?php
 require_once 'controlador/get.controller.php';
 //echo '<pre>';print_r($routesArray[1]);echo '</pre>';
-//Separar tabla
-$table = explode("?", $routesArray[1])[0];
+
 //echo '<pre>';print_r($routesArray[1]);echo '</pre>';
 
 //$select = "*";
@@ -12,20 +11,135 @@ $response = new GetController();
 
 //Encaso de buscar con get con filtro (where)
 if(isset($_GET['linkTo']) && isset($_GET["equalTo"])){
+    
     $campo = $_GET['linkTo'];
     $value = $_GET["equalTo"];
     //echo '<pre>';print_r($campo);echo '</pre>';
     //echo '<pre>';print_r($value);echo '</pre>';
     
-    $response->getDataFilter($table, $select, $campo, $value);
+    //validar peticiones para usuarios autorizados
+    if(isset($_GET["token"])){
+   
+    $tableToken = $_GET['table'] ?? "clients";
+    $suffix = $_GET['suffix'] ?? "client";
+    //Verificar si el token no ha expirado devuelve : "vigente_token", "expiro_token", "no_existe_token"
+    $validate = Conexion::tokenValidate($_GET['token'], $tableToken, $suffix);
 
+                    //Token valido
+                    if($validate == "vigente_token"){
+
+                    $response->getDataFilter($table, $select, $campo, $value);
+
+                    }
+                    //Error token expiro
+                    if($validate == "expiro_token"){
+                    
+                            $json = array(
+                                'status' => 303,
+                                'Results' => 'Error: the token has expired'
+                    
+                            );
+                    
+                            echo json_encode($json, http_response_code($json['status']));
+                    
+                            return; 
+                        
+                    }
+        
+                    //Error token no coincide en base de datos 
+                    if($validate == "no_existe_token"){
+                            
+                            $json = array(
+                                'status' => 400,
+                                'Results' => 'Error: the token not auth'
+                    
+                            );
+                    
+                            echo json_encode($json, http_response_code($json['status']));
+                    
+                            return; 
+        
+                    }
+                    
+     //Error no suministraron token 
+    }else{
+            
+        $json = array(
+            'status' => 400,
+            'Results' => 'Error: auth required'
+
+        );
+
+        echo json_encode($json, http_response_code($json['status']));
+
+        return; 
+}                
+
+//Buscar por porcentaje
 }elseif(isset($_GET['name']) && isset($_GET["porcentaje"])){
+   
     $name = $_GET['name'];
     $porcentaje = $_GET['porcentaje'];
     //echo '<pre>';print_r($name);echo '</pre>';
     //echo '<pre>';print_r($porcentaje);echo '</pre>';
 
-    $response->getDataPorcentaje($select, $name, $porcentaje);
+    //validar peticiones para usuarios autorizados
+   if(isset($_GET["token"])){
+   
+    $tableToken = $_GET['table'] ?? "clients";
+    $suffix = $_GET['suffix'] ?? "client";
+    //Verificar si el token no ha expirado devuelve : "vigente_token", "expiro_token", "no_existe_token"
+    $validate = Conexion::tokenValidate($_GET['token'], $tableToken, $suffix);
+
+                    //Token valido
+                    if($validate == "vigente_token"){
+
+                        $response->getDataPorcentaje($select, $name, $porcentaje);
+                    }
+                    
+                    //Error token expiro
+                    if($validate == "expiro_token"){
+                    
+                                $json = array(
+                                    'status' => 303,
+                                    'Results' => 'Error: the token has expired'
+                        
+                                );
+                        
+                                echo json_encode($json, http_response_code($json['status']));
+                        
+                         return; 
+                            
+                    }
+            
+                    //Error token no coincide en base de datos 
+                    if($validate == "no_existe_token"){
+                                
+                                $json = array(
+                                    'status' => 400,
+                                    'Results' => 'Error: the token not auth'
+                        
+                                );
+                        
+                                echo json_encode($json, http_response_code($json['status']));
+                        
+                        return; 
+            
+                    }                
+    
+    //Error no suministraron token 
+    }else{
+                
+        $json = array(
+            'status' => 400,
+            'Results' => 'Error: auth required'
+
+        );
+
+        echo json_encode($json, http_response_code($json['status']));
+
+        return; 
+    }                    
 
 }else{
     //Encaso de buscar con get sin filtro (where)
